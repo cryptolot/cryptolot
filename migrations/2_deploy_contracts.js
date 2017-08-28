28cea719899ea923aa463cbc32f9a5c5f9f8b57d
Alex Grozav <alex@grozav.com>
2017-08-28 23:40:04 +0300

    Migrating crowdsale along with token.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,42 @@
-var Lottery = artifacts.require("./Lottery.sol");
+var Token = artifacts.require("./Token.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
 
-var initialAmount = 1000000;
-var tokenName = 'Lottery';
-var tokenSymbol = 'LTRY';
-var decimalUnits = 3;
+// Token configuration
+//
+var _tokenInitialAmount = 1000000;
+var _tokenName = 'Lottery';
+var _tokenSymbol = 'LTRY';
+var _tokenDecimalUnits = 3;
+
+
+// Crowdsale configuration
+//
+var _crowdsaleBeneficiary = 0x0;
+var _crowdsaleFundingGoal = 1000;
+var _crowdsaleAmount = _tokenInitialAmount / 2;
+var _crowdsalePrice = _crowdsaleAmount / _crowdsaleFundingGoal;
+var _crowdsaleStartTime = Date.now();
+var _crowdsaleEndTime = new Date();
+_crowdsaleEndTime.setDate(_crowdsaleEndTime.getDate() + 1);
+_crowdsaleEndTime = _crowdsaleEndTime.getTime();
+
 
 module.exports = function(deployer) {
-  deployer.deploy(Lottery, initialAmount, tokenName, tokenSymbol, decimalUnits);
+  deployer.deploy(
+    Token,
+    _tokenInitialAmount,
+    _tokenName,
+    _tokenSymbol,
+    _tokenDecimalUnits
+  ).then(function() {
+    return deployer.deploy(
+      Crowdsale,
+      _crowdsaleBeneficiary,
+      _crowdsaleFundingGoal,
+      _crowdsalePrice,
+      _crowdsaleStartTime,
+      _crowdsaleEndTime,
+      Token.address
+    ).then(() => {console.log(Crowdsale)});
+  });
 };
