c86570e3b1f519f1f005172d1387e708f02ed514
Alex Grozav <alex@grozav.com>
2017-08-29 23:27:36 +0300

    Added token  inheritance contracts for minting, burning, upgrading and owning a token..

---
+++ contracts/token/Owned.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.11;
+
+// Owned contract, some functionality is accessible exclusively by the contract
+// owner. Provides basic access functionality.
+//
+contract Owned {
+  address public owner;
+
+  // Sets the owner of the contract to be the contract creator
+  //
+  function Owned() {
+    owner = msg.sender;
+  }
+
+  // Allows only the contract owner to call the modified contract function
+  //
+  modifier onlyOwner {
+    require (msg.sender == owner);
+    _;
+  }
+
+  // Transfers contract ownership
+  //
+  function transferOwnership(address newOwner) onlyOwner {
+    owner = newOwner;
+  }
+}
