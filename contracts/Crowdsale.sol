6668dfda08c720e0640ea4843a2dff68b5b12b4c
Alex Grozav <alex@grozav.com>
2017-08-29 23:28:06 +0300

    Updated crowdsale to have payout during finalize session.

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,25 +1,60 @@
 pragma solidity ^0.4.11;
 
 
-import './Token.sol';
+import './lib/SafeMathLib.sol';
+import './token/Owned.sol';
+import './LotteryToken.sol';
 
 
 // Crowdsale contract
 //
-contract Crowdsale {
+contract Crowdsale is Owned {
+
+
+  using SafeMathLib for uint;
+
+
   address public beneficiary;
   uint public amountRaised;
   uint public fundingGoal;
   uint public price;
   uint public startTime;
   uint public endTime;
-  Token public tokenReward;
+  LotteryToken public tokenReward;
 
-  mapping(address => uint256) public balanceOf;
+
+  // Crowdsale Statistics
+  //
+  uint256 public tokensLimit;
+  uint256 public tokensIssued;
+  uint256 public weiRefunded;
+
+
+  // Crowdsale state
+  //
+  enum State{Unknown, Preparing, PreFunding, Funding, Success, Failure, Finalized, Refunding}
+  State state;
+
+
+  // List of investor addresses, used when finalizing the crowdsale
+  //
+  address[] public addresses;
+
+
+  // Tokens to be issued to the given address
+  //
+  mapping(address => uint256) public tokens;
+
+
+  // Amount funded by each address
+  //
+  mapping(address => uint256) public invested;
+
 
 
   bool fundingGoalReached = false;
   bool crowdsaleOpen = false;
+
 
   // Crowdsale contract constructor
   //
@@ -28,15 +63,20 @@ contract Crowdsale {
   // @param _price Price of each token in Ethereun
   // @param _startTime Crowdsale starting time
   // @param _endTime Crowdsale ending time
+  // @param _tokensLimit Token limit after which minting starts
   // @param _tokenReward Address of the token used as reward
   //
-  function Crowdsale(address _beneficiary, uint _fundingGoal, uint _price, uint _startTime, uint _endTime, Token _tokenReward) {
+  function Crowdsale(address _beneficiary, uint _fundingGoal, uint _price, uint _startTime, uint _endTime, uint256 _tokensLimit, LotteryToken _tokenReward) {
     beneficiary = _beneficiary;
     fundingGoal = _fundingGoal * 1 ether;
     price = _price * 1 ether;
     startTime = _startTime;
     endTime = _endTime;
-    tokenReward = Token(_tokenReward);
+    tokensLimit = _tokensLimit;
+    tokenReward = LotteryToken(_tokenReward);
+    state = State.Preparing;
+
+    require(startTime < endTime);
   }
 
 
@@ -45,9 +85,17 @@ contract Crowdsale {
   //
   function () payable duringCrowdsale {
     uint amount = msg.value;
-    balanceOf[msg.sender] += amount;
-    amountRaised += amount;
-    tokenReward.transfer(msg.sender, amount / price);
+    uint tokensAmount = amount.div(price);
+
+    // Store details about crowdsale investors
+    if (invested[msg.sender] == 0) addresses.push(msg.sender);
+    invested[msg.sender] = invested[msg.sender].plus(amount);
+    tokens[msg.sender] = tokens[msg.sender].plus(tokensAmount);
+
+    // Update crowdsale statistics
+    amountRaised = amountRaised.plus(amount);
+    tokensIssued = tokensIssued.plus(tokensAmount);
+
     FundTransfer(msg.sender, amount, true);
   }
 
@@ -65,7 +113,6 @@ contract Crowdsale {
   }
 
 
-
   // Withdraw the amount raised after the crowdsale
   //
   function withdraw() afterCrowdsale {
@@ -73,6 +120,73 @@ contract Crowdsale {
       require(beneficiary.send(amountRaised));
       FundTransfer(beneficiary, amountRaised, false);
     }
+  }
+
+
+  // Finalize a succcesful crowdsale.
+  //
+  // The owner can trigger a call to the contract that provides post-crowdsale
+  // actions, like releasing the tokens.
+  //
+  function finalize() public onlyOwner {
+    // Already finalized
+    require(!inState(State.Finalized));
+
+    // Total number of tokens transferred until now, used to check whether to
+    // transfer or mint tokens
+    uint totalTokensTransferred = 0;
+
+    // Finalizing is optional. We only call it if we are given a finalizing agent.
+    //
+    for (uint i = 0; i < addresses.length; i++) {
+      uint tokensToTransfer = tokens[addresses[i]];
+
+      if (tokensToTransfer == 0) continue;
+      totalTokensTransferred = totalTokensTransferred.plus(tokensToTransfer);
+
+      // Start minting tokens if crowdsale token limit reached
+      if (totalTokensTransferred <= tokensLimit) {
+        tokenReward.transfer(addresses[i], tokensToTransfer);
+      } else {
+        tokenReward.mint(addresses[i], tokensToTransfer);
+      }
+    }
+
+    setState(State.Finalized);
+  }
+
+
+  // Investors can claim refund.
+  //
+  // Note that any refunds from proxy buyers should be handled separately,
+  // and not through this contract.
+  //
+  function refund() public {
+    require(inState(State.Refunding));
+
+    uint256 weiValue = invested[msg.sender];
+
+    require(weiValue != 0);
+
+    invested[msg.sender] = 0;
+    weiRefunded = weiRefunded.plus(weiValue);
+
+    Refund(msg.sender, weiValue);
+    require(msg.sender.send(weiValue));
+  }
+
+
+  // Start refund period if the crowdsale doesn't succeed
+  //
+  function setState(State _state) public onlyOwner {
+    state = _state;
+  }
+
+
+  // Start refund period if the crowdsale doesn't succeed
+  //
+  function inState(State _state) constant returns (bool _inState) {
+    return state == _state;
   }
 
 
@@ -92,4 +206,5 @@ contract Crowdsale {
   event GoalReached(address _beneficiary, uint _amountRaised);
   event FundTransfer(address _backer, uint _amount, bool _isContribution);
   event ReceivedApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData);
+  event Refund(address _to, uint _amount);
 }
