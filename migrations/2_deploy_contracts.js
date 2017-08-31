9ebac03b49a379a153a7959545a0093d876a3ce8
Alex Grozav <alex@grozav.com>
2017-09-01 00:09:16 +0300

    Updated main token migration.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,46 +1,49 @@
-var Token = artifacts.require("./LotteryToken.sol");
-var Crowdsale = artifacts.require("./Crowdsale.sol");
+/**
+ * Dependencies
+ */
+const path = require('path');
+
+/**
+ * Contracts and libraries
+ */
 var SafeMathLib = artifacts.require("./lib/SafeMathLib.sol");
+var TokenStorage = artifacts.require("./storage/TokenStorage.sol");
+var Token = artifacts.require("./StandardToken.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
 
-// Token configuration
-//
-var _tokenInitialAmount = 1000000;
-var _tokenName = 'Lottery';
-var _tokenSymbol = 'LTRY';
-var _tokenDecimalUnits = 3;
+/**
+ * Contracts configuration
+ */
+var config = {
+  token: require(path.join(__dirname, '..', 'config', 'LotteryToken'))
+}
 
-
-// Crowdsale configuration
-//
-var _crowdsaleBeneficiary = 0x0;
-var _crowdsaleFundingGoal = 1000;
-var _crowdsaleTokenLimit = _tokenInitialAmount / 2;
-var _crowdsalePrice = _crowdsaleFundingGoal / _crowdsaleTokenLimit;
-var _crowdsaleStartTime = Date.now();
-var _crowdsaleEndTime = new Date();
-_crowdsaleEndTime.setDate(_crowdsaleEndTime.getDate() + 1);
-_crowdsaleEndTime = _crowdsaleEndTime.getTime();
 
 module.exports = function(deployer) {
+  // Deploy and link safe math library
   deployer.deploy(SafeMathLib);
-  deployer.link(SafeMathLib, [ Token, Crowdsale ]);
+  deployer.link(SafeMathLib, [ TokenStorage, Token, Crowdsale ]);
 
-  deployer.deploy(
-    Token,
-    _tokenInitialAmount,
-    _tokenName,
-    _tokenSymbol,
-    _tokenDecimalUnits
-  ).then(function() {
-    return deployer.deploy(
-      Crowdsale,
-      _crowdsaleBeneficiary,
-      _crowdsaleFundingGoal,
-      _crowdsalePrice,
-      _crowdsaleStartTime,
-      _crowdsaleEndTime,
-      _crowdsaleTokenLimit,
-      Token.address
-    );
+
+  // Deploy token storage and initialize total supply of tokens.
+  deployer.deploy(TokenStorage, config.token.totalSupply).then(() => {
+    return TokenStorage.deployed();
+  }).then((_tokenStorage) => {
+    // Deploy token and link to deployed token storage.
+    deployer.deploy(Token, config.token.name, config.token.symbol, config.token.decimals, TokenStorage.address).then(() => {
+      // Add the token contract as a token storage module to allow it to
+      // interact with the storage.
+      _tokenStorage.setModule(Token.address, true);
+
+      return Token.deployed();
+    });
   });
+
+  // deployer.then(() => {
+  //   return TokenStorage.deployed(config.token.totalSupply);
+  // }).then((_tokenStorage) => {
+  //   return LotteryToken.deployed(config.token.name, config.token.symbol, config.token.decimals, _tokenStorage.address)
+  // }).then((_lotteryToken) => {
+  //   console.log("IT WORKED!!----",_lotteryToken.address)
+  // });
 };
