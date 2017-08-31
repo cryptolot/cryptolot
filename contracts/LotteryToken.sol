d126a91a6a7f2b55c9ab8b259a70c6b79f5d679d
Alex Grozav <alex@grozav.com>
2017-09-01 00:07:38 +0300

    Removed duplicate balances name which was failing deployment.

--- contracts/LotteryToken.sol
+++ contracts/LotteryToken.sol
@@ -2,13 +2,15 @@ pragma solidity ^0.4.11;
 
 
 import "./lib/SafeMathLib.sol";
-import "./token/BurnableToken.sol";
+import "./token/StandardToken.sol";
+/*import "./token/BurnableToken.sol";
 import "./token/MintableToken.sol";
-import "./token/UpgradeableToken.sol";
+import "./token/UpgradeableToken.sol";*/
 
 
-contract LotteryToken is UpgradeableToken, MintableToken, BurnableToken {
+/*contract LotteryToken is UpgradeableToken, MintableToken, BurnableToken {*/
+contract LotteryToken is StandardToken {
   using SafeMathLib for uint;
 
-  function LotteryToken(uint256 _initialAmount, string _tokenName, string _tokenSymbol, uint8 _decimalUnits) StandardToken(_initialAmount, _tokenName, _tokenSymbol, _decimalUnits) {}
+  function LotteryToken(string _name, string _symbol, uint8 _decimals, address _tokenStorage) StandardToken(_name, _symbol, _decimals, _tokenStorage) {}
 }
