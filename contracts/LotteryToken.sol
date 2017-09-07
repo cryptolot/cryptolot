53068f9534596a18373c6b61037aa960f59b29e9
Alex Grozav <alex@grozav.com>
2017-09-07 23:13:37 +0300

    Removed constructor.

--- contracts/LotteryToken.sol
+++ contracts/LotteryToken.sol
@@ -3,14 +3,10 @@ pragma solidity ^0.4.11;
 
 import "./lib/SafeMathLib.sol";
 import "./token/StandardToken.sol";
-/*import "./token/BurnableToken.sol";
 import "./token/MintableToken.sol";
+/*import "./token/BurnableToken.sol";
 import "./token/UpgradeableToken.sol";*/
 
 
 /*contract LotteryToken is UpgradeableToken, MintableToken, BurnableToken {*/
-contract LotteryToken is StandardToken {
-  using SafeMathLib for uint;
-
-  function LotteryToken(string _name, string _symbol, uint8 _decimals, address _tokenStorage) StandardToken(_name, _symbol, _decimals, _tokenStorage) {}
-}
+contract LotteryToken is StandardToken, MintableToken {}
